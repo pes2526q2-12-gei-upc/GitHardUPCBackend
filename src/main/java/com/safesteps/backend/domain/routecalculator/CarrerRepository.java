d55226b09c3e57aa3df4fff9e407a663915bedad
oriol.berruezo <oriol.berruezo@estudiantat.upc.edu>
2026-03-08 18:43:03 +0100

    [TG-17] feat: Comments

--- src/main/java/com/safesteps/backend/domain/routecalculator/CarrerRepository.java
+++ src/main/java/com/safesteps/backend/domain/routecalculator/CarrerRepository.java
@@ -10,6 +10,11 @@ import java.util.List;
 @Repository
 public interface CarrerRepository extends JpaRepository<Carrer, Long> {
 
+    /* Query per a trobar la ruta mes curta.
+     Utilitza la funcio pgr_dijkstra de PostGIS per a calcular la ruta mes curta entre dos nodes.
+     Tambe utilitza una vista (v_trams_nodes) que conté les dades dels carrers (id, source, target, longitud)
+     creuant els carrers amb els nodes de la BD.
+     */
     @Query(value =
             "SELECT node FROM pgr_dijkstra" +
                 "(" +
@@ -25,6 +30,13 @@ public interface CarrerRepository extends JpaRepository<Carrer, Long> {
             @Param("destId") Long destId
     );
 
+    /*
+    Query per a trobar el node mes proper a unes coordenades donades (latitud i longitud).
+     Utilitza les funcions de PostGIS per a calcular la distancia entre les coordenades donades i les coordenades dels nodes de la BD.
+     Donada una entrada en lat. i long. retorna el FID del node mes proper a aquestes coordenades.
+     Com la BD esta en UTM (EPSG:25831) i les coordenades d'entrada estan en WGS84 (EPSG:4326),
+     es necessari transformar les coordenades d'entrada a UTM abans de calcular la distancia.
+    * */
     @Query(value = "SELECT \"FID\" FROM bcn_grafvial_nodes " +
             "ORDER BY ST_Distance(" +
             "  ST_SetSRID(ST_MakePoint(\"Coord_X\", \"Coord_Y\"), 25831), " +
