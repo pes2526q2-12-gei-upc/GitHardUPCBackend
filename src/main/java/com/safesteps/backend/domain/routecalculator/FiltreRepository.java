f114fc756a602a42f7eb240d29d2d3b39dc8c30e
oriol.berruezo <oriol.berruezo@estudiantat.upc.edu>
2026-04-23 15:52:14 +0200

    [TG-49] fix: Set filter repository to extend Filtre,String. Then set the @Param to googleId

--- src/main/java/com/safesteps/backend/domain/routecalculator/FiltreRepository.java
+++ src/main/java/com/safesteps/backend/domain/routecalculator/FiltreRepository.java
@@ -3,8 +3,11 @@ package com.safesteps.backend.domain.routecalculator;
 import com.safesteps.backend.domain.routecalculator.projections.FiltreDBProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface FiltreRepository extends JpaRepository<Carrer, Long> {
+@Repository
+public interface FiltreRepository extends JpaRepository<Filtre, String> {
     @Query(value = "SELECT * FROM filtres WHERE google_id = :googleId", nativeQuery = true)
-    FiltreDBProjection findByGoogleId(String googleId);
+    FiltreDBProjection findByGoogleId(@Param("googleId") String googleId);
 }
