191ce4ca544d6d324d780b3aa37a0df2472a9b86
Oscar <oscar.lopez.andreu@estudiantat.upc.edu>
2026-04-24 18:09:36 +0200

    [TG-00] fix: error fix rnf

--- src/test/java/com/safesteps/backend/RouteCalculatorPerformanceTest.java
+++ src/test/java/com/safesteps/backend/RouteCalculatorPerformanceTest.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("integration")
+@ActiveProfiles("local")
 @Tag("integration")
 class RouteCalculatorPerformanceTest {
 
