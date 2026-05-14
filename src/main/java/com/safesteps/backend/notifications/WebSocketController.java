95f391f55ac34b6daad553e40b85826aeb8a2446
oriol.berruezo <oriol.berruezo@estudiantat.upc.edu>
2026-05-14 13:34:54 +0200

    [TG-42] fix: tret alguns endpoints de ws ja que utilitzarem API REST

--- src/main/java/com/safesteps/backend/notifications/WebSocketController.java
+++ src/main/java/com/safesteps/backend/notifications/WebSocketController.java
@@ -12,23 +12,7 @@ public class WebSocketController {
 
     /*
     Per a fer la implementacio, canviar string msg pel dto corresponent
-    Es molt probable que nomes ens quedem amb location.update i ja.
      */
-    @MessageMapping("/chat.send")
-    public void handleChat(@Payload String msg, Principal principal) {
-        throw new NotImplementedException();
-    }
-
-    @MessageMapping("/group-chat.send")
-    public void handleGroupChat(@Payload String msg, Principal principal) {
-        throw new NotImplementedException();
-    }
-
-    @MessageMapping("/emergency.start")
-    public void handleEmergency(@Payload String msg, Principal principal) {
-        throw new NotImplementedException();
-    }
-
     @MessageMapping("/location.update")
     public void handleLocation(@Payload String msg, Principal principal) {
         throw new NotImplementedException();
