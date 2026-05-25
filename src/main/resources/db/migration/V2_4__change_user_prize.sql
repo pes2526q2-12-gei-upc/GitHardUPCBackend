acd0c099883c0ea1123d878f8a5e9eba0226efc4
oriol.berruezo <oriol.berruezo@estudiantat.upc.edu>
2026-05-25 20:40:41 +0200

    [TG-00] fix: DatabaseUpdateScheduler to take the .properties set by cl. Changed one user prize from R008_RTCOL_R57G255B20 to R008_RTCOL_R149G255B36

---
+++ src/main/resources/db/migration/V2_4__change_user_prize.sql
@@ -0,0 +1,3 @@
+DELETE FROM public.prizes WHERE id IN ('R008_RTCOL_R57G255B20');
+
+INSERT INTO public.prizes (id, name, url, oddity) VALUES ('R008_RTCOL_R149G255B36', 'Ruta de color lima', 'NONE', 'RARE');
